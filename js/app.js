c21728baf5aa63c39cca90941de1a5f02d0ebcbd
ornery-mouse <cory.sorel@gmail.com>
2021-10-19 19:57:05 -0500

    Chapter 5 Complete - Made the buddies

--- js/app.js
+++ js/app.js
@@ -120,10 +120,17 @@ function makeBuddies() {
 	console.log('5: makeBuddies')
 
 	// create an `aside` tag
-
+	const aside = document.createElement('aside')
 	// put an `unordered list` of the `'buddies'` in the aside
-
+	const goodGuys = document.createElement('ul')
+	for (i = 0; i < buddies.length; i++){
+		const li = document.createElement('li')
+		li.innerText = buddies[i]
+		aside.appendChild(li)
+	}
+	aside.appendChild(goodGuys)
 	// insert your aside as a child element of `rivendell`
+	document.getElementById('Rivendell').appendChild(aside)
 }
 
 // COMMIT YOUR WORK
