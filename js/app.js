ab7838cc98f992669822073ecc87611f29da85eb
nrissman <nrissman90@gmail.com>
2022-06-10 14:51:49 -0400

    chapter 5 complete- made buddies

--- js/app.js
+++ js/app.js
@@ -138,7 +138,18 @@ function makeBuddies() {
 	// put an `unordered list` of the `'buddies'` in the aside
 
 	// insert your aside as a child element of `rivendell`
-
+	let aside = document.createElement("aside")
+	aside.setAttribute("class", "buddies")
+	const ul = document.createElement("ul")
+	for (let i = 0; i < buddies.length; i++) {
+		let li = document.createElement("li")
+		li.setAttribute("id", buddies[i])
+		li.setAttribute("class", "buddy")
+		ul.appendChild(li)
+		li.innerText = buddies[i]
+		console.log(li)
+	}
+	document.getElementById(lands[1]).appendChild(ul)
 }
 
 // COMMIT YOUR WORK
