cd1197095cc88c7bfcf86f6e170d1c2d4a75078e
nguyenpham207 <seanphamgd@gmail.com>
2022-09-12 12:45:03 -0700

    Chapter 1

--- js/app.js
+++ js/app.js
@@ -17,23 +17,28 @@ const lands = ['The-Shire', 'Rivendell', 'Mordor']
 // ============
 // Chapter 1
 // ============
-
+	// 1. create a section tag with an id of middle-earth
 function makeMiddleEarth() {
+	let middleEarth = document.createElement('selection')
+	middleEarth.setAttribute('id', "middle-earth")
 	console.log('1: makeMiddleEarth')
 
-	// 1. create a section tag with an id of middle-earth
-
 	// 2. use a for loop to iterate over the lands array that does the following:
-
-	//   2a. creates an article tag (there should be one for each land when the loop is done)
-
-	//   2b. gives each land article an `id` tag of the corresponding land name
-
+for (let i = 0; i < lands.length; i++) {
+	let land = document.createElement('article')
+	/   2b. gives each land article an `id` tag of the corresponding land name
+	land.setAttribute('id',lands[i])
 	//   2c. includes an h1 with the name of the land inside each land article
+	const landName = document.createElement('h1')
+	landName.innerText = land[i]
+	land.appendChild(landName)
 
 	//   2d. appends each land to the middle-earth section
-
+	middleEarth.appendChild(land)
 	// 3. append the section to the body of the DOM.
+	const body = document.querySelector('body')
+	body.appendChild(middleEarth)
+}
 }
 
 // COMMIT YOUR WORK
