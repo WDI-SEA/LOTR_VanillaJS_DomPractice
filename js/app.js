df90cf0edbffa1de90a15a648b94c8a8e9660305
thepjos <josh.thephasdin@gmail.com>
2022-12-16 14:57:44 -0600

    Chapter 1 complete - Made Middle Earth

--- js/app.js
+++ js/app.js
@@ -8,9 +8,11 @@ const hobbits = [
 	`Peregrin "Pippin" Took`,
 ]
 
-const buddies = ['Gandalf the Grey', 'Legolas', 'Gimli', 'Strider', 'Boromir']
+const buddies = ['Gandalf the Grey', 
+'Legolas', 'Gimli', 'Strider', 'Boromir']
 
-const baddies = ['Sauron', 'Saruman', 'The Uruk-hai', 'Orcs']
+const baddies = ['Sauron', 'Saruman', 
+'The Uruk-hai', 'Orcs']
 
 const lands = ['The-Shire', 'Rivendell', 'Mordor']
 
@@ -22,18 +24,23 @@ function makeMiddleEarth() {
 	console.log('1: makeMiddleEarth')
 
 	// 1. create a section tag with an id of middle-earth
-
+	const section = document.createElement('section')
+	section.id = 'middle-earth'
 	// 2. use a for loop to iterate over the lands array that does the following:
-
-	//   2a. creates an article tag (there should be one for each land when the loop is done)
-
-	//   2b. gives each land article an `id` tag of the corresponding land name
-
-	//   2c. includes an h1 with the name of the land inside each land article
-
-	//   2d. appends each land to the middle-earth section
-
-	// 3. append the section to the body of the DOM.
+	for (let i = 0; i < lands.length; i++) {
+		//   2a. creates an article tag (there should be one for each land when the loop is done)
+		const landArticle = document.createElement('article')
+		//   2b. gives each land article an `id` tag of the corresponding land name
+		landArticle.id = lands[i]
+		//   2c. includes an h1 with the name of the land inside each land article
+		const landName = document.createElement('h1')
+		landName.textContent = lands[i]
+		landArticle.appendChild(landName)
+		//   2d. appends each land to the middle-earth section
+		section.appendChild(landArticle)
+	}		
+		// 3. append the section to the body of the DOM.
+	document.body.appendChild(section)
 }
 
 // COMMIT YOUR WORK
